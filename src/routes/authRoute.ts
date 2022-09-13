583ba5a1f95094532f0fb005cad9e8bf04d7a7d4
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-13 12:03:08 -0300

    feat: create auth Schemas

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { validateSchemaMiddleware } from "../middlewares/schemaMiddleware";
+import { signInSchema, signUpSchema } from "../schemas/authSchema";
 
 const authRouter = Router();
 
-authRouter.post("/signup");
-authRouter.post("/signin");
+authRouter.post("/signup", validateSchemaMiddleware(signUpSchema), () => console.log('oi') );
+authRouter.post("/signin", validateSchemaMiddleware(signInSchema));
 
 export default authRouter;
---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,12 @@
+import joi from "joi";
+
+export const signUpSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  confirmPassword: joi.ref("password"),
+});
+
+export const signInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
