d22c96146a15737c2f300a40214a7f8ed6f4be15
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-13 11:43:29 -0300

    feat: add router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./routes/index";
+import handleErrorsMiddleware from "./middlewares/handleErrorsMiddleware";
 
 dotenv.config();
 
@@ -9,6 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
+app.use(handleErrorsMiddleware);
 
 const { PORT } = process.env || 5000;
 app.listen(PORT, () => {
