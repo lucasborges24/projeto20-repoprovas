9e06548e8c6c668ce73b26f8e6e7b6c8bc2e6a0a
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-16 17:12:59 -0300

    chore: export test functions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { ensureAuthenticatedMiddleware } from "../middlewares/authMiddleware";
 import authRouter from "./authRoute";
+import testRouter from "./testRoute";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(ensureAuthenticatedMiddleware);
+router.use(testRouter);
 
 export default router;
