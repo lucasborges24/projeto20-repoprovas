ee5f070ae2f1c5635721ce38e62c40d2d6742a78
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-16 12:54:04 -0300

    feat: add POST signUp/signIn ROUTE

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { authController } from "../controllers";
 import { validateSchemaMiddleware } from "../middlewares/schemaMiddleware";
 import { signInSchema, signUpSchema } from "../schemas/authSchema";
 
 const authRouter = Router();
 
-authRouter.post("/signup", validateSchemaMiddleware(signUpSchema), () => console.log('oi') );
-authRouter.post("/signin", validateSchemaMiddleware(signInSchema));
+authRouter.post(
+  "/signup",
+  validateSchemaMiddleware(signUpSchema),
+  authController.signUp
+);
+authRouter.post(
+  "/signin",
+  validateSchemaMiddleware(signInSchema),
+  authController.signIn
+);
 
 export default authRouter;
